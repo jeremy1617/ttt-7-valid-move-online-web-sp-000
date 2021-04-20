6830590c3b677d44728fcc348a176960498e930f
curious-analyst-4243 <jeremycperry@gmail.com>
2021-04-20 17:57:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, space)
-  if position_taken? && board[space].between(0,9)?
+  if position_taken? and board[space].between(0,9)?
     puts "good move"
   else
     puts "not vaild"
